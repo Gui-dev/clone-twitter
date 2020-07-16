4b8b8d796e0b54a53be2d4d0c3e1932fe3ec538d
Gui-dev <adrianon10@gmail.com>
2020-07-16 18:48:51 -0300

    Iniciando as configurações e estilos gerais

---
+++ src/style/GlobalStyle.ts
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+
+  :root {
+    --primary: #000;
+    --secondary: #15181C;
+    --search: #202327;
+    --white: #D9D9D9;
+    --gray: #7A7A7A;
+    --outline: #2F3336;
+    --retweet: #00C06B;
+    --like: #E8265E;
+    --twitter: #33A1F2;
+    --twitter-dark-hover: #011017;
+    --twitter-light-hover: #2C8ED6;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    color: var(--white);
+  }
+
+  html {
+    background-color: var(--primary);
+  }
+
+  html, body, #root {
+    font-size: 62.5%;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    width: 100vw;
+    height: 100vh;
+    max-width: 100vw;
+    max-height: 100vh;
+  }
+
+  *, button, input {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    border: 0;
+    background: none;
+  }
+`
